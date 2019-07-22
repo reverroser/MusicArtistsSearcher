e022786d92e6db1a01cd09d9d5f953ef99771327
Roser Reverte <reverroser@gmail.com>
2019-07-22 22:13:04 +0200

    Initial Commit

---
+++ MusicArtistsSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicArtistsSearcher
+//
+//  Created by Roser Reverte Avila on 22/07/2019.
+//  Copyright © 2019 Roser Reverte Avila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
