928435d5c651b87a2a1b79b27a8312390e7ed162
Roser Reverte <reverroser@gmail.com>
2019-07-26 07:09:33 +0200

    fix

--- MusicArtistsSearcher/API/APIClient.swift
+++ MusicArtistsSearcher/API/APIClient.swift
@@ -16,7 +16,7 @@ class APIClient<T: Decodable> {
      * This function does a GET request to the Itunes API
      * T is equal to the data model of the response. Example: [MusicArtist]
      */
-    func fetch(apiRequest: APIRequest) -> Observable<T> {
+    func send(apiRequest: APIRequest) -> Observable<T> {
         return Observable<T>.create { [unowned self] observer in
             let request = apiRequest.request(with: self.baseURL)
             let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
--- MusicArtistsSearcher/MusicArtistsSearch/MusicArtistsSearchAPI.swift
+++ MusicArtistsSearcher/MusicArtistsSearch/MusicArtistsSearchAPI.swift
@@ -16,7 +16,7 @@ class MusicArtistsSearchAPI {
     // The request returns { resultsCount: Int, results: Array }
     func searchMusicArtists(searchText: String) -> Observable<[MusicArtist]> {
         return Observable<[MusicArtist]>.create { [unowned self] observer in
-            self.apiClient.fetch(apiRequest: MusicArtistsSearchRequest(term: searchText.lowercased()))
+            self.apiClient.send(apiRequest: MusicArtistsSearchRequest(term: searchText.lowercased()))
                 .subscribe(
                     onNext: { data in
                         observer.onNext(data.results!)
